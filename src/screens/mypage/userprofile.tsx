1a0af92f70cc4469cee3d3240061870068552785
Dochaki <49172536+kdc9050@users.noreply.github.com>
2024-03-13 19:37:50 +0900

    뱃지  map 수정

--- src/screens/mypage/userprofile.tsx
+++ src/screens/mypage/userprofile.tsx
@@ -234,14 +234,20 @@ const UserProfile = () => {
             </UserProfileMainIntroduceTopContainer>
             <UserProfileMainIntroduceBottomContainer>
               <h1>보유 뱃지</h1>
-
               <div className="imgcnt">
-                {userProfo.badges && userProfo.badges.length > 1 ? (
+                {userProfo.badges &&
+                Array.isArray(userProfo.badges) &&
+                userProfo.badges.length > 0 &&
+                userProfo.badges[0] !== "" ? (
                   userProfo.badges.map((badge: string) => (
                     <img key={badge} src={badgeImageMap[badge]} alt="" />
                   ))
-                ) : (
+                ) : userProfo.badges &&
+                  Array.isArray(userProfo.badges) &&
+                  userProfo.badges.length > 0 ? (
                   <p>보유한 뱃지가 없습니다.</p>
+                ) : (
+                  <p>뱃지 정보가 없습니다.</p>
                 )}
               </div>
             </UserProfileMainIntroduceBottomContainer>
