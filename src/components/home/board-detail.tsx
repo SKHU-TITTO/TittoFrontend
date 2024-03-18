bbb3d3fa916830e35e871e1ed34d6ce1056e1535
Dochaki <49172536+kdc9050@users.noreply.github.com>
2024-03-18 09:55:05 +0900

    컴포넌트 나머지 쓸 모없는 코드 수정

--- src/components/home/board-detail.tsx
+++ src/components/home/board-detail.tsx
@@ -36,39 +36,6 @@ const categoryToString = (category: string) => {
   }
 };
 
-const BoardWrapper = styled.div`
-  padding-top: 10px;
-  width: 100%;
-  color: #bababa;
-  border-bottom: 2px solid #bababa;
-  padding-bottom: 10px;
-
-  .category {
-    font-size: 15px;
-    font-weight: bold;
-    padding-bottom: 10px;
-  }
-  .title {
-    color: black;
-    font-size: 18px;
-  }
-  .detail {
-    height: 30px;
-    font-weight: 100;
-    font-size: 13px;
-    padding-top: 10px;
-    padding-bottom: 5px;
-    color: #4d4d4d;
-  }
-  span {
-    display: inline-block;
-  }
-  .show-comment {
-    font-size: 0.9em;
-    color: #bababa;
-  }
-`;
-
 const HBoardDetail = ({
   category,
   title,
@@ -108,3 +75,36 @@ const HBoardDetail = ({
 };
 
 export default HBoardDetail;
+
+const BoardWrapper = styled.div`
+  padding-top: 10px;
+  width: 100%;
+  color: #bababa;
+  border-bottom: 2px solid #bababa;
+  padding-bottom: 10px;
+
+  .category {
+    font-size: 15px;
+    font-weight: bold;
+    padding-bottom: 10px;
+  }
+  .title {
+    color: black;
+    font-size: 18px;
+  }
+  .detail {
+    height: 30px;
+    font-weight: 100;
+    font-size: 13px;
+    padding-top: 10px;
+    padding-bottom: 5px;
+    color: #4d4d4d;
+  }
+  span {
+    display: inline-block;
+  }
+  .show-comment {
+    font-size: 0.9em;
+    color: #bababa;
+  }
+`;
